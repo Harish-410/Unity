6b8d1697bc4e531a6e999d0745233185949de559
Harish Appanapalli <harisha@sti.com>
2023-06-15 10:02:46 +0530

    first commit

---
+++ Assets/Scripts/AddNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddNumbers : MonoBehaviour
+{
+    int number_1 = Random.Range(0, 100);
+    int number_2 = Random.Range(0, 100);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int AddTwoNumbers()
+    {
+        return number_1 + number_2;
+    }
+}
---
+++ Assets/Scripts/PrintNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintNumbers : MonoBehaviour
+{
+
+   // public int number = Random.Range(0, 100);
+    public int number = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GenerateRandomNumber()
+    {
+        return 10;
+    }
+}
